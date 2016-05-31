246b6bc4dd6e90be4246d2f6f5d8def911660fc6
Sergey Koltsov <by3dring@gmail.com>
2016-05-31 22:52:21 +0500

    fogotten files :'D

---
+++ app/src/main/java/com/ringov/testandroid/view/friends_list/FriendsListCallBack.java
@@ -0,0 +1,6 @@
+package com.ringov.testandroid.view.friends_list;
+
+public interface FriendsListCallBack {
+    void callLogout();
+    void callClearCache();
+}
---
+++ app/src/main/java/com/ringov/testandroid/view/login/LoginCallBack.java
@@ -0,0 +1,6 @@
+package com.ringov.testandroid.view.login;
+
+public interface LoginCallBack {
+    void callLogin();
+    void callLoginOffline();
+}
